0236a05b540dc51150c7b6d6fefa9daede1b8ff6
fred-andree.oja <fred-oja@outlook.com>
2024-04-11 15:35:18 +0300

    feat: add validation tests

--- api/src/test/java/demo/service/validation/filter_validator/FilterDtoValidatorTest.java
+++ api/src/test/java/demo/service/validation/filter_validator/FilterDtoValidatorTest.java
@@ -4,6 +4,7 @@ import demo.controller.dto.FilterDto;
 import demo.service.filter.DataTypes;
 import demo.service.filter.DateCriteria;
 import demo.service.filter.NumberCriteria;
+import demo.service.filter.StringCriteria;
 import demo.service.validation.ValidationResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.mockito.MockitoAnnotations;
 import java.time.Instant;
 
 import static demo.service.validation.filter_validator.FilterErrors.INVALID_CRITERIA_ERROR;
+import static demo.service.validation.filter_validator.FilterErrors.INVALID_DATE_VALUE;
 import static demo.service.validation.filter_validator.FilterErrors.INVALID_NUMBER_VALUE;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -57,12 +59,26 @@ class FilterDtoValidatorTest {
         assertEquals(INVALID_CRITERIA_ERROR, result.getMessage());
     }
 
-
     @Test
-    void validateDateValues_shouldReturnFalse_whenValueNotValid() {
+    void validateDateValues_shouldReturnFalse_whenValueisNotValid() {
+        FilterDto filterDto = new FilterDto()
+                .setType(DataTypes.DATE)
+                .setCriteria(DateCriteria.BEFORE.getKood())
+                .setValue("abc");
+        ValidationResult result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(INVALID_DATE_VALUE, result.getMessage());
 
+        filterDto.setValue("123");
+        result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(INVALID_DATE_VALUE, result.getMessage());
+
+        filterDto.setValue(null);
+        result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(INVALID_DATE_VALUE, result.getMessage());
     }
-
 
     @Test
     void validateNumeric_shouldReturnTrue() {
@@ -76,10 +92,42 @@ class FilterDtoValidatorTest {
     }
 
     @Test
-    void validateNumericCriteria_shouldReturnFalse() {
+    void validateNumeric_shouldReturnFalse_whenCriteriaInvalid() {
+        FilterDto filterDto = new FilterDto()
+                .setType(DataTypes.NUMBER)
+                .setCriteria(DateCriteria.BEFORE.getKood())
+                .setValue("2");
+        ValidationResult result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(INVALID_CRITERIA_ERROR, result.getMessage());
+
+        filterDto.setCriteria("invalidCriteria");
+        result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(INVALID_CRITERIA_ERROR, result.getMessage());
+    }
+
+    @Test
+    void validateNumeric_shouldReturnFalse_whenValueInvalid() {
         FilterDto filterDto = new FilterDto()
                 .setType(DataTypes.NUMBER)
                 .setCriteria(NumberCriteria.SMALLER_THAN.getKood())
+                .setValue("2as");
+        ValidationResult result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(INVALID_NUMBER_VALUE, result.getMessage());
+
+        filterDto.setValue(null);
+        result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(INVALID_NUMBER_VALUE, result.getMessage());
+    }
+
+    @Test
+    void validateString_shouldReturnTrue() {
+        FilterDto filterDto = new FilterDto()
+                .setType(DataTypes.STRING)
+                .setCriteria(StringCriteria.CONTAINS.getKood())
                 .setValue("1");
         ValidationResult result = filterDtoValidator.validate(filterDto);
         assertTrue(result.isValid());
@@ -87,13 +135,19 @@ class FilterDtoValidatorTest {
     }
 
     @Test
-    void validateNumericValue_shouldReturnFalse() {
+    void validateString_shouldReturnFalse_whenCriteriaInvalid() {
         FilterDto filterDto = new FilterDto()
-                .setType(DataTypes.NUMBER)
-                .setCriteria(NumberCriteria.SMALLER_THAN.getKood())
-                .setValue("A");
+                .setType(DataTypes.STRING)
+                .setCriteria(DateCriteria.BEFORE.getKood())
+                .setValue("as2");
         ValidationResult result = filterDtoValidator.validate(filterDto);
         assertFalse(result.isValid());
-        assertEquals(INVALID_NUMBER_VALUE, result.getMessage());
+        assertEquals(INVALID_CRITERIA_ERROR, result.getMessage());
+
+        filterDto.setCriteria("invalidCriteria");
+        result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(INVALID_CRITERIA_ERROR, result.getMessage());
     }
+
 }
