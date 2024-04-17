b31c0bbf24ed0bdf6f51ca9620d81cd60754df32
fred-andree.oja <fred-oja@outlook.com>
2024-04-17 16:51:26 +0300

    feat: add string filtering unit tests and date composite tests

--- api/src/test/java/demo/service/filter/FilteringLogicServiceTest.java
+++ api/src/test/java/demo/service/filter/FilteringLogicServiceTest.java
@@ -12,11 +12,11 @@ import static demo.service.filter.DateCriteria.AFTER;
 import static demo.service.filter.DateCriteria.BEFORE;
 import static demo.service.filter.DateCriteria.EQUALS;
 import static demo.service.filter.UserFieldNames.DOB;
+import static demo.service.filter.UserFieldNames.NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FilteringLogicServiceTest extends ContextIntegrationTest {
   User mainUser;
-
 
   @BeforeEach
   void beforeEach() {
@@ -24,6 +24,7 @@ class FilteringLogicServiceTest extends ContextIntegrationTest {
     mainUser = users.get(0);
   }
 
+  // DATE
   @Test
   void filteringByDate_After_shouldReturnList() {
     FilterDto f1 = new FilterDto().setCriteria(AFTER.getKood()).setValue("2001-05-10T21:00:25.451157400Z").setType(DataTypes.DATE).setFieldName(DOB);
@@ -94,4 +95,87 @@ class FilteringLogicServiceTest extends ContextIntegrationTest {
     assertEquals(1, test_v3.size());
     assertEquals("qJohn Does", test_v3.get(0).getName());
   }
+
+  @Test
+  void filteringByDate_composite_shouldReturnList() {
+    FilterDto f1 = new FilterDto().setCriteria(AFTER.getKood()).setValue("1996-04-10T21:00:25.451157400Z").setType(DataTypes.DATE).setFieldName(DOB);
+    FilterDto f2 = new FilterDto().setCriteria(BEFORE.getKood()).setValue("2009-04-10T21:00:25.451157400Z").setType(DataTypes.DATE).setFieldName(DOB);
+    var userFilter_v2 = createUserFilter(List.of(f1, f2), "user filter 1", mainUser);
+
+    var test_v2 = filteringLogicService.findAllByFilter(userFilter_v2, User.class);
+    assertEquals(2, test_v2.size());
+    assertEquals("qJane Does", test_v2.get(0).getName());
+    assertEquals("qJack Doesn't", test_v2.get(1).getName());
+  }
+
+  // STRING
+  @Test
+  void filteringByString_contains_shouldReturnList() {
+    FilterDto f1 = new FilterDto().setCriteria(StringCriteria.CONTAINS.getKood()).setValue("Does").setType(DataTypes.STRING).setFieldName(NAME);
+    var userFilter = createUserFilter(List.of(f1), "user filter 1", mainUser);
+
+    var res = filteringLogicService.findAllByFilter(userFilter, User.class);
+    assertEquals(3, res.size());
+  }
+
+  @Test
+  void filteringByString_contains_shouldReturnEmptyList() {
+    FilterDto f1 = new FilterDto().setCriteria(StringCriteria.CONTAINS.getKood()).setValue("abcd").setType(DataTypes.STRING).setFieldName(NAME);
+    var userFilter = createUserFilter(List.of(f1), "user filter 1", mainUser);
+
+    var res = filteringLogicService.findAllByFilter(userFilter, User.class);
+    assertEquals(0, res.size());
+  }
+
+  @Test
+  void filteringByString_does_not_contain_shouldReturnList() {
+    FilterDto f1 = new FilterDto().setCriteria(StringCriteria.DOES_NOT_CONTAIN.getKood()).setValue("Does").setType(DataTypes.STRING).setFieldName(NAME);
+    var userFilter = createUserFilter(List.of(f1), "user filter 1", mainUser);
+
+    var res = filteringLogicService.findAllByFilter(userFilter, User.class);
+    assertEquals(1, res.size());
+  }
+
+  @Test
+  void filteringByString_equals_shouldReturnList() {
+    FilterDto f1 = new FilterDto().setCriteria(StringCriteria.EQUALS.getKood()).setValue("qJack Doesn't").setType(DataTypes.STRING).setFieldName(NAME);
+    var userFilter = createUserFilter(List.of(f1), "user filter 1", mainUser);
+
+    var res = filteringLogicService.findAllByFilter(userFilter, User.class);
+    assertEquals(1, res.size());
+  }
+
+  @Test
+  void filteringByString_equals_shouldEmptyList() {
+    FilterDto f1 = new FilterDto()
+            .setCriteria(StringCriteria.EQUALS.getKood())
+            .setValue("qJack Doesn")
+            .setType(DataTypes.STRING)
+            .setFieldName(NAME);
+    var userFilter = createUserFilter(List.of(f1), "user filter 1", mainUser);
+
+    var res = filteringLogicService.findAllByFilter(userFilter, User.class);
+    assertEquals(0, res.size());
+  }
+
+//  @ParameterizedTest
+//  @MethodSource("filterCriteriaProvider")
+//  void filterByString_ShouldReturnCorrectList(StringCriteria criteria, String value, int expectedSize) {
+//    FilterDto f1 = new FilterDto().setCriteria(criteria.getKood()).setValue(value).setType(DataTypes.STRING).setFieldName(NAME);
+//    var userFilter = createUserFilter(List.of(f1), "user filter 1", mainUser);
+//
+//    var res = filteringLogicService.findAllByFilter(userFilter, User.class);
+//    assertEquals(expectedSize, res.size());
+//  }
+//
+//  private static Stream<Arguments> filterCriteriaProvider() {
+//    return Stream.of(
+//            Arguments.of(StringCriteria.CONTAINS, "Does", 3),
+//            Arguments.of(StringCriteria.CONTAINS, "abcd", 0),
+//            Arguments.of(StringCriteria.DOES_NOT_CONTAIN, "Does", 1),
+//            Arguments.of(StringCriteria.EQUALS, "qJack Doesn't", 1),
+//            Arguments.of(StringCriteria.EQUALS, "qJack Doesn", 0)
+//    );
+//  }
+
 }
