e296d5c2c0a30d906be3799c3a7dff9f1e916019
fred-andree.oja <fred-oja@outlook.com>
2024-04-19 14:40:53 +0300

    feat: implement paratereized tests

--- api/src/test/java/demo/service/validation/UserNameValidatorTest.java
+++ api/src/test/java/demo/service/validation/UserNameValidatorTest.java
@@ -3,13 +3,21 @@ package demo.service.validation;
 import demo.model.User;
 import demo.repository.UserRepository;
 import demo.service.validation.user_validator.NameValidator;
+import demo.service.validation.user_validator.UserErrors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.stream.Stream;
+
+import static demo.service.filter.DateCriteria.AFTER;
+import static demo.service.filter.DateCriteria.BEFORE;
+import static demo.service.filter.DateCriteria.EQUALS;
 import static demo.service.validation.user_validator.UserErrors.NAME_DOESNT_CONTAIN_Q;
 import static demo.service.validation.user_validator.UserErrors.NAME_NOT_UNIQUE;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,33 +36,9 @@ class UserNameValidatorTest {
     userNameValidator = new NameValidator(userRepository);
   }
 
-  @Test
-  void testValidate_ValidUser() {
-    User user = new User().setName("validqusername");
-    when(userRepository.existsByName(anyString())).thenReturn(false);
-
-    ValidationResult result = userNameValidator.validate(user);
-
-    assertTrue(result.isValid());
-    assertNull(result.getMessage());
-    verify(userRepository, times(1)).existsByName("validqusername");
-  }
-
-  @Test
-  void testValidate_NonUniqueName() {
-    User user = new User().setName("duplicatequsername");
-    when(userRepository.existsByName(anyString())).thenReturn(true);
-
-    ValidationResult result = userNameValidator.validate(user);
-
-    assertFalse(result.isValid());
-    assertEquals(NAME_NOT_UNIQUE, result.getMessage());
-    verify(userRepository, times(1)).existsByName("duplicatequsername");
-  }
-
   @ParameterizedTest
-  @ValueSource(strings = {"validqusername", "anotherqexample.com"})
-  void testValidate_ValidUserName(String userName) {
+  @MethodSource("validUserNames")
+  void nameValidation_shouldReturnTrue_whenValidName(String userName) {
     User user = new User().setName(userName);
     when(userRepository.existsByName(userName)).thenReturn(false);
 
@@ -64,14 +48,29 @@ class UserNameValidatorTest {
     assertNull(result.getMessage());
   }
 
+  static Stream<String> validUserNames() {
+    return Stream.of(
+            "validqusername",
+            "anotherqexample.com"
+    );
+  }
+
   @ParameterizedTest
-  @ValueSource(strings = {"noatsymbol", "invalid.email.com"})
-  void testValidate_InvalidUserName(String userName) {
-    User user = new User().setName(userName);
+  @MethodSource("invalidUserNames")
+  void nameValidation_shouldReturnFalse_whenInvalidName(UserErrors userErrors, String input) {
+    User user = new User().setName(input);
+    when(userRepository.existsByName("duplicatequsername")).thenReturn(true);
 
     ValidationResult result = userNameValidator.validate(user);
-
     assertFalse(result.isValid());
-    assertEquals(NAME_DOESNT_CONTAIN_Q, result.getMessage());
+    assertEquals(userErrors, result.getMessage());
+  }
+
+  private static Stream<Arguments> invalidUserNames() {
+    return Stream.of(
+            Arguments.of(NAME_DOESNT_CONTAIN_Q, "noatsymbol"),
+            Arguments.of(NAME_DOESNT_CONTAIN_Q, "invalid.email.com"),
+            Arguments.of(NAME_NOT_UNIQUE, "duplicatequsername")
+    );
   }
 }
---
+++ api/src/test/java/demo/service/validation/filter_validator/FilterValidatorDateTest.java
@@ -0,0 +1,87 @@
+package demo.service.validation.filter_validator;
+
+import demo.controller.dto.FilterDto;
+import demo.service.filter.DataTypes;
+import demo.service.filter.DateCriteria;
+import demo.service.filter.NumberCriteria;
+import demo.service.validation.ValidationResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockitoAnnotations;
+
+import java.time.Instant;
+import java.util.stream.Stream;
+
+import static demo.service.validation.filter_validator.FilterErrors.INVALID_CRITERIA_ERROR;
+import static demo.service.validation.filter_validator.FilterErrors.INVALID_DATE_VALUE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FilterValidatorDateTest {
+
+    private FilterDtoValidator filterDtoValidator;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        filterDtoValidator = new FilterDtoValidator();
+    }
+
+    @Test
+    void validateDateValues_shouldReturnTrue() {
+        FilterDto filterDto = new FilterDto()
+                .setType(DataTypes.DATE)
+                .setCriteria(DateCriteria.BEFORE.getKood())
+                .setValue(Instant.now().toString());
+        ValidationResult result = filterDtoValidator.validate(filterDto);
+        assertTrue(result.isValid());
+        assertNull(result.getMessage());
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidDateCriteria")
+    void validateDateValues_shouldReturnFalse(String criteria, FilterErrors error) {
+        FilterDto filterDto = new FilterDto()
+                .setType(DataTypes.DATE)
+                .setCriteria(criteria)
+                .setValue(Instant.now().toString());
+        ValidationResult result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(error, result.getMessage());
+    }
+
+    private static Stream<Arguments> invalidDateCriteria() {
+        return Stream.of(
+                Arguments.of(NumberCriteria.SMALLER_THAN.getKood(), INVALID_CRITERIA_ERROR),
+                Arguments.of("invalidCriteria", INVALID_CRITERIA_ERROR),
+                Arguments.of(null, INVALID_CRITERIA_ERROR)
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidDateValues")
+    void validateDateValues_shouldReturnFalse_whenValueIsNotValid(String value, FilterErrors error) {
+        FilterDto filterDto = new FilterDto()
+                .setType(DataTypes.DATE)
+                .setCriteria(DateCriteria.BEFORE.getKood())
+                .setValue(value);
+        ValidationResult result = filterDtoValidator.validate(filterDto);
+        assertFalse(result.isValid());
+        assertEquals(error, result.getMessage());
+    }
+
+    private static Stream<Arguments> invalidDateValues() {
+        return Stream.of(
+                Arguments.of("abc", INVALID_DATE_VALUE),
+                Arguments.of("123", INVALID_DATE_VALUE),
+                Arguments.of(null, INVALID_DATE_VALUE)
+
+        );
+    }
+}
