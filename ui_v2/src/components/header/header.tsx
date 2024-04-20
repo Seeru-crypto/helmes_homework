599241692c3873f626dd9634c2c6b8a68adffa4c
Seeru <puupuha@gmail.com>
2024-04-20 14:11:26 +0300

    feat: add  persistant state
    Took 13 minutes

--- ui_v2/src/components/header/header.tsx
+++ ui_v2/src/components/header/header.tsx
@@ -4,12 +4,10 @@ import {useNavigate} from "react-router-dom";
 function Header() {
     const navigate = useNavigate();
 
-
     function navigateToPage({ event, destination }: { event: React.MouseEvent<HTMLElement, MouseEvent>; destination: string }): void {
         event.preventDefault();
         navigate(`${destination}`);
     }
-
 
   return (
       <div className={styles.header}>
--- ui_v2/src/entities/users/users.reducer.ts
+++ ui_v2/src/entities/users/users.reducer.ts
@@ -1,23 +1,23 @@
 import axios from 'axios';
-import {createAsyncThunk, isFulfilled, isPending} from '@reduxjs/toolkit';
-import {createEntitySlice, EntityState, IQueryParams} from "../../store/reducer.utils.ts";
-import {ASC} from "../../util/pagination.constants.ts";
-import {defaultValue, IUser} from "../../store/user.model.ts";
+import {createAsyncThunk, createSlice, isFulfilled} from '@reduxjs/toolkit';
+import {IQueryParams} from "../../store/reducer.utils.ts";
+import {IUser} from "../../store/user.model.ts";
 
-const initialState: EntityState<IUser> = {
-    loading: false,
-    errorMessage: null,
-    entities: [],
-    entity: defaultValue,
-    updating: false,
-    updateSuccess: false,
+
+interface Iusers {
+    users: IUser[];
+}
+
+
+const initialState: Iusers = {
+    users: []
 };
 
 const apiUrl = 'api/regions';
 
 // Actions
 
-export const getEntities = createAsyncThunk('region/fetch_entity_list', async ({ sort }: IQueryParams) => {
+export const getEntities = createAsyncThunk('region/fetch_entity_list', async ({sort}: IQueryParams) => {
     const requestUrl = `${apiUrl}?${sort ? `sort=${sort}&` : ''}cacheBuster=${new Date().getTime()}`;
     return axios.get<IUser[]>(requestUrl);
 });
@@ -25,36 +25,19 @@ export const getEntities = createAsyncThunk('region/fetch_entity_list', async ({
 
 // slice
 
-export const UserSlice = createEntitySlice({
+export const UserSlice = createSlice({
     name: 'user',
     initialState,
+    reducers: {},
     extraReducers(builder) {
         builder
             .addMatcher(isFulfilled(getEntities), (state, action) => {
-                const { data } = action.payload;
-
-                return {
-                    ...state,
-                    loading: false,
-                    entities: data.sort((a, b) => {
-                        if (!action.meta?.arg?.sort) {
-                            return 1;
-                        }
-                        const order = action.meta.arg.sort.split(',')[1];
-                        const predicate = action.meta.arg.sort.split(',')[0];
-                        return order === ASC ? (a[predicate] < b[predicate] ? -1 : 1) : b[predicate] < a[predicate] ? -1 : 1;
-                    }),
-                };
+                state.users = action.payload.data;
             })
-            .addMatcher(isPending(getEntities), state => {
-                state.errorMessage = null;
-                state.updateSuccess = false;
-                state.loading = true;
-            })
-    },
-});
+    }
+})
 
-export const { reset } = UserSlice.actions;
+export const {} = UserSlice.actions;
 
 // Reducer
 export default UserSlice.reducer;
