fa47ff0b21d9014c4d17db348e1450098f0106da
Seeru <puupuha@gmail.com>
2024-09-06 16:18:51 +0300

    feat: fix tests
    Took 24 minutes

--- api/src/test/resources/test-data.sql
+++ api/src/test/resources/test-data.sql
@@ -2,3 +2,11 @@ TRUNCATE sector, "user", user_sectors CASCADE;
 
 ALTER SEQUENCE sector_id_seq RESTART WITH 1;
 ALTER SEQUENCE user_sectors_id_seq RESTART WITH 1;
+
+TRUNCATE filter_options cascade;
+ALTER SEQUENCE filter_options_id_seq RESTART WITH 1;
+
+insert into filter_options (id, field, field_type)
+values (1, 'name', 'STRING'),
+       (2, 'dob', 'DATE'),
+       (3, 'height', 'NUMBER');
