8c13008fa62650bec56400ed43f2b6ac8e42e1f6
Seeru <puupuha@gmail.com>
2024-09-06 16:45:01 +0300

    feat: implement csv test files
    Took 11 minutes

--- api/src/test/java/demo/service/validation/UserNameValidatorTest.java
+++ api/src/test/java/demo/service/validation/UserNameValidatorTest.java
@@ -5,24 +5,13 @@ import demo.repository.UserRepository;
 import demo.service.validation.user_validator.NameValidator;
 import demo.service.validation.user_validator.UserErrors;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.stream.Stream;
-
-import static demo.service.filter.DateCriteria.AFTER;
-import static demo.service.filter.DateCriteria.BEFORE;
-import static demo.service.filter.DateCriteria.EQUALS;
-import static demo.service.validation.user_validator.UserErrors.NAME_DOESNT_CONTAIN_Q;
-import static demo.service.validation.user_validator.UserErrors.NAME_NOT_UNIQUE;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 class UserNameValidatorTest {
 
   @Mock
@@ -37,7 +26,7 @@ class UserNameValidatorTest {
   }
 
   @ParameterizedTest
-  @MethodSource("validUserNames")
+  @CsvFileSource(resources = "/validUserNames.csv")
   void nameValidation_shouldReturnTrue_whenValidName(String userName) {
     User user = new User().setName(userName);
     when(userRepository.existsByName(userName)).thenReturn(false);
@@ -48,15 +37,8 @@ class UserNameValidatorTest {
     assertNull(result.getMessage());
   }
 
-  static Stream<String> validUserNames() {
-    return Stream.of(
-            "validqusername",
-            "anotherqexample.com"
-    );
-  }
-
   @ParameterizedTest
-  @MethodSource("invalidUserNames")
+  @CsvFileSource(resources = "/inValidUserNames.csv")
   void nameValidation_shouldReturnFalse_whenInvalidName(UserErrors userErrors, String input) {
     User user = new User().setName(input);
     when(userRepository.existsByName("duplicatequsername")).thenReturn(true);
@@ -64,13 +46,5 @@ class UserNameValidatorTest {
     ValidationResult result = userNameValidator.validate(user);
     assertFalse(result.isValid());
     assertEquals(userErrors, result.getMessage());
-  }
-
-  private static Stream<Arguments> invalidUserNames() {
-    return Stream.of(
-            Arguments.of(NAME_DOESNT_CONTAIN_Q, "noatsymbol"),
-            Arguments.of(NAME_DOESNT_CONTAIN_Q, "invalid.email.com"),
-            Arguments.of(NAME_NOT_UNIQUE, "duplicatequsername")
-    );
   }
 }
